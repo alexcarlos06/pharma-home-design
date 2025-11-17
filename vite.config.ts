de10b81390795322bb43307207633248ee57fff4
alexcarlos06 <alexcsousa2011@hotmail.com>
2025-11-16 22:39:06 -0300

    Ajuste na configuração de deploy do site

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { componentTagger } from 'lovable-tagger'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'serve' ? '/' : '/pharma-home-design/',
+    server: {
+      host: '0.0.0.0',
+      port: 8080,
     },
-  },
-}));
+    plugins: [
+      react(),
+      command === 'serve' && componentTagger()
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  }
+})
